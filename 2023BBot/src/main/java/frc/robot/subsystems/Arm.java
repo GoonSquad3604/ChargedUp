d817a05b1e08720e8858ca7271208b5afaac7ea8
Goon Squad <richard.budop@gmail.com>
2023-01-31 17:26:31 -0500

    arm setup

--- 2023BBot/src/main/java/frc/robot/subsystems/Arm.java
+++ 2023BBot/src/main/java/frc/robot/subsystems/Arm.java
@@ -4,22 +4,41 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.sensors.CANCoderStatusFrame;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxAbsoluteEncoder;
+import com.revrobotics.SparkMaxAlternateEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.ArmConstants;
 
 public class Arm extends SubsystemBase {
 
   private CANSparkMax shoulder;
   private CANSparkMax elbow;
   private static Arm _instance;
+  private SparkMaxAlternateEncoder encoder;
   
   /** Creates a new Arm. */
   public Arm() {
     shoulder = new CANSparkMax(Constants.ArmConstants.shoulderID, MotorType.kBrushless);
     elbow = new CANSparkMax(Constants.ArmConstants.elbowID, MotorType.kBrushless);
+    //encoder = elbow.getAlternateEncoder(, 0);
+  }
+
+  public static double[] getArmAngles(int height, int distance) {
+
+    // Angle for elbow
+    double elbowAngle = Math.acos((Math.pow(distance, 2) + Math.pow(height, 2) - Math.pow(Constants.ArmConstants.bottomArmLength, 2) - Math.pow(Constants.ArmConstants.topArmLength, 2))/2.0*Constants.ArmConstants.bottomArmLength*Constants.ArmConstants.topArmLength)
+    / 2.0*Constants.ArmConstants.bottomArmLength*Constants.ArmConstants.topArmLength;
+    // Angle for shoulder
+    double shoulderAngle = (Math.atan(height/distance)) + Math.atan((Constants.ArmConstants.topArmLength*Math.sin(elbowAngle))/(Constants.ArmConstants.bottomArmLength + Constants.ArmConstants.topArmLength*Math.cos(elbowAngle)));
+
+    double angles[] = {shoulderAngle, elbowAngle};
+    return angles;
   }
 
   public static final Arm getInstance() {
